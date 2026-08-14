06b023b2259c0accde487c8522b22dfa06b5baaa
James <jfcodex@gmail.com>
2026-08-13 20:24:28 -0500

    test: implemented the 'Get Test' test

--- file_test.go
+++ file_test.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
+
+var globalSink []byte
 
 // var bruh sync.Once
 
@@ -31,7 +34,7 @@ func BenchmarkTests(b *testing.B) {
 		key := make([][]byte, 1000)
 		value := make([][]byte, 1000)
 
-		for i := 0; i < b.N; i++ {
+		for i := 0; i < 1000; i++ {
 			key[i] = []byte(fmt.Sprintf("User-%v", i))
 			value[i] = []byte(fmt.Sprintf("User-%v", i))
 		}
@@ -42,11 +45,48 @@ func BenchmarkTests(b *testing.B) {
 		childB.RunParallel(func(setPb *testing.PB) {
 			iteration := 0
 			for setPb.Next() {
-				iteration := iteration % len(key)
+				iteration = iteration % len(key)
 				testBookMap.Set(key[iteration], value[iteration])
 				iteration++
 			}
 		})
+	})
 
+	b.Run("Get Test", func(childB *testing.B) {
+		testBookMap := NewContactBookMap()
+		mutex := sync.Mutex{}
+
+		key := make([][]byte, 1000)
+		value := make([][]byte, 1000)
+
+		for i := 0; i < 1000; i++ {
+			key[i] = []byte(fmt.Sprintf("User-%v", i))
+			value[i] = []byte(fmt.Sprintf("User-%v", i))
+			testBookMap.Set(key[i], value[i])
+		}
+
+		childB.ResetTimer()
+		childB.ReportAllocs()
+
+		childB.RunParallel(func(getPb *testing.PB) {
+			var localFaucet []byte
+			localDst := make([]byte, 1024)
+			iteration := 0
+
+			// still gets allocated | 10 * (1024) B / b.N (# of operations) rounded to integers
+
+			for getPb.Next() {
+				iteration = iteration % len(key)
+				faucet, status := testBookMap.Get(key[iteration], localDst)
+				if status {
+					localFaucet = faucet
+				}
+				iteration++
+			}
+			mutex.Lock()
+			globalSink = localFaucet
+			mutex.Unlock()
+
+		})
 	})
 }
