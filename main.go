1bade7dffe98bfac30b93e1ba798f84d036c44f6
James <jfcodex@gmail.com>
2026-08-23 17:51:30 -0500

    perf: refactored server.go into a more idiomatic approach, utilized struct, fixed edge-cases with shutdown

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"context"
 	"errors"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -11,40 +11,91 @@ import (
 	"syscall"
 )
 
-func main() {
-	contactBook := NewContactBookMap()
-	wg := sync.WaitGroup{}
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+type Server struct {
+	listener     net.Listener // listener interface
+	listenerAddr string       // port
+	storage      *contactBookMap
+	isClosed     bool
 
-	ctx, cancel := context.WithCancel(context.Background())
+	mu    sync.Mutex
+	connM map[net.Conn]struct{}
+}
 
-	listener, err := net.Listen("tcp", ":8080")
+func NewServer(listenerAddr string, storage *contactBookMap) *Server {
+	return &Server{
+		listenerAddr: listenerAddr,
+		storage:      storage,
+		connM:        make(map[net.Conn]struct{}),
+	}
+}
+
+func (s *Server) Start() error {
+	var err error
+
+	osSignalChan := make(chan os.Signal, 1)
+	waitGroup := sync.WaitGroup{}
+
+	s.listener, err = net.Listen("tcp", s.listenerAddr)
 	if err != nil {
-		log.Fatal("Failed to listen", err)
+		return err
 	}
 
-	go func() {
-		<-shutdown
-		log.Println("Server Shutdown")
-		cancel()
-		listener.Close()
+	signal.Notify(osSignalChan, syscall.SIGTERM, syscall.SIGINT)
+	defer signal.Stop(osSignalChan)
 
+	go func() {
+		<-osSignalChan
+		log.Println("Server Shutdown")
+		s.Close()
 	}()
 
 	for {
-		conn, err := listener.Accept() // performs the three-way handshake under the hood
+		conn, err := s.listener.Accept() // performs the three-way handshake under the hood
 		if err != nil {
-			if errors.Is(err, net.ErrClosed) {
-				log.Printf("Error: %v", err)
+			if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) { // client disconnect or server shutdown
 				break
 			}
-			log.Println("Failed to secure connection", err)
+			log.Printf("Connection Error %v\n", err)
 			continue
 		}
-		wg.Add(1)
-		go process(conn, contactBook, ctx, &wg)
+		s.mu.Lock()
+		if s.isClosed { // reads valeu
+			s.mu.Unlock()
+			conn.Close()
+			continue
+		}
+		s.connM[conn] = struct{}{}
+		s.mu.Unlock()
+		waitGroup.Add(1)
+		go process(s, conn, s.storage, &waitGroup)
 	}
 
-	wg.Wait()
+	waitGroup.Wait()
+	return nil
+}
+
+func (s *Server) Close() {
+
+	s.listener.Close()
+
+	s.mu.Lock()
+	s.isClosed = true
+	cons := make([]net.Conn, 0, len(s.connM)) // reading the map
+	for k := range s.connM {                  // only keys
+		cons = append(cons, k)
+	}
+	s.mu.Unlock()
+
+	for _, conn := range cons {
+		conn.Close() // to unblock and wake up conn.Read()
+	}
+}
+
+func main() {
+	contactBook := NewContactBookMap()
+	serverAddr := NewServer(":3000", contactBook)
+	if err := serverAddr.Start(); err != nil {
+		log.Fatalf("Server exited with error: %v", err)
+	}
+	log.Println("Server gracefully shutdown")
 }
